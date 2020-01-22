1f1fcf0e9593a96712792333aad1c0c9759ee490
David Öhlin <hello@davidohl.in>
2020-01-22 14:38:21 +0100

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,18 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'index.js',
 	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js'],
+	},
 	devServer: {
 		contentBase: path.resolve(__dirname, 'public'),
 	},
