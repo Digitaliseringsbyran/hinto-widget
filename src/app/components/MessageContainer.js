ae862c2aed7206d12ba0a63009a58d7483a9a131
David Öhlin <hello@davidohl.in>
2020-02-05 16:32:57 +0100

    wip messagecontainer

--- src/app/components/MessageContainer.js
+++ src/app/components/MessageContainer.js
@@ -1,18 +1,29 @@
 import { h } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
+import styled from 'styled-components'
+
+const TIMER = 2000
 
 const MessageContainer = ({ message }) => {
-	const [text, set] = useState(null)
+	const [visible, set] = useState(false)
 
 	useEffect(() => {
-		set(message)
+		set(true)
 
 		setTimeout(() => {
-			set(null)
-		}, 2000)
+			set(false)
+		}, TIMER)
 	}, [message])
 
-	return text ? <div>{text}</div> : null
+	if (!visible) {
+		return null
+	}
+
+	return <Container>hello</Container>
 }
+
+const Container = styled.div`
+	border: 1px solid red;
+`
 
 export default MessageContainer
