21c7f0f1eb0e24a8daadd07d3695a2eaa3b2ab9e
Albin Martinsson <albinmartinsson91@gmail.com>
2020-02-07 10:28:46 +0100

    Add avatar component, typing indicator and message container

--- src/app/components/Message.js
+++ src/app/components/Message.js
@@ -1,7 +1,17 @@
 import { h } from 'preact'
+import styled from 'styled-components'
 
 const Message = ({ text }) => {
-	return <div>{text}</div>
+	return <Container>{text}</Container>
 }
 
 export default Message
+
+const Container = styled.div`
+	padding: 14px;
+	border-radius: 4px;
+	background: white;
+	border: 1px solid rgba(0, 0, 0, 0.04);
+	box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.04),
+		0px 16px 24px rgba(0, 0, 0, 0.04), 0px 24px 32px rgba(0, 0, 0, 0.04);
+`
